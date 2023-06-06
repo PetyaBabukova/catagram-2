9e6fbe632a6d4b44630ff066849f5276b3869a41
Petya Babukova <p_babukova@abv.bg>
2023-06-06 17:15:21 +0300

    Add Persistance

--- cats.js
+++ cats.js
@@ -1,15 +1,23 @@
-
-const cats = [];
+const fs = require('fs');
+const catsData = require('./cats.json');
+const cats = catsData.slice();
 
 function add(name) {
     cats.push(name); 
-}
+    fs.writeFile('./cats.json',JSON.stringify(cats), ()=>{
+        if(err){
+            console.log('some error');
+        }
+
+        console.log('successful write');
+    });
+};
 
 function getAll() {
     return cats.slice();
-}
+};
 
 module.exports = {
     add,
     getAll
-}
+};
