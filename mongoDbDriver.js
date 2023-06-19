7f439aee949b2589b1016587cced05a78da12d61
Petya Babukova <p_babukova@abv.bg>
2023-06-19 17:35:52 +0300

    MongoDB demo

---
+++ mongoDbDriver.js
@@ -0,0 +1,43 @@
+// const { MongoClient } = require('mongodb');
+
+// async function main() {
+//     // we'll add code here soon
+//     const uri = 'mongodb://localhost:27017';
+//     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    
+//     await client.connect();
+// };
+
+
+
+
+
+
+
+
+
+// This is old, it doesn`t work for mongo 6
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+const uri = 'mongodb://localhost:27017';
+const client = new MongoClient(uri, {useUnifiedTopology: true,
+    family: 4,
+  });
+  
+client.connect((err) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+    
+            let db = client.db('catagram');
+            let cats = db.collection('cats');
+    
+            cats.findOne({}, (err, result)=>{
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                console.log(result);
+            })
+        });
