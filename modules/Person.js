f30821f1799c4bff4ceb46ff1975c2cfa36b379a
Petya Babukova <p_babukova@abv.bg>
2023-06-21 15:16:50 +0300

    Add virtual properties

---
+++ modules/Person.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+});
+
+personSchema.methods.getInfo = function () {
+    console.log(`Hello my name is ${this.name} and I am ${this.age} old!`);
+};
+
+// VIRTUAL properties - they dont save in the DB
+personSchema.virtual('birthYear') 
+.get(function () {
+    // get current year - age
+    return 2023 - this.age;
+})
+// .set()
+
+//validate - see in the presentation
+
+module.exports = mongoose.model('Person', personSchema);
--- mongooseExample.js
+++ mongooseExample.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Person = require('./modules/Person')
 const uri = 'mongodb://localhost:27017/mongotest';
 
 mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true, family: 4,});
@@ -8,13 +9,6 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', ()=>{
     console.log("Connected to database!");
 });
-
-const personSchema = new mongoose.Schema({
-    name: String,
-    age: Number,
-});
-
-const Person = mongoose.model('Person', personSchema);
 
 let person = new Person({name: "Petya", age: 33});
 
@@ -39,7 +33,13 @@ let person = new Person({name: "Petya", age: 33});
 // };
 // save();
 
-// second way - promise
-person.save().then((res)=>{
-   console.log(res);
-});
+// // second way - promise
+// person.save().then((res)=>{
+//    console.log(res);
+// });
+
+// how to use virtual properties
+Person.find({})
+.then((people)=>{
+people.forEach(x => console.log(`I am born ${x.birthYear}`));
+})
